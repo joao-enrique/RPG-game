4096e9feb85c73e8cc6a8bbca0725a4664039b3f
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-10-28 15:10:22 -0300

    💾Feat: add Stacking items

--- Scripts/inventoryItems.gd
+++ Scripts/inventoryItems.gd
@@ -4,3 +4,4 @@ class_name InventoryItems
 
 @export var name: String = ""
 @export var texture = Texture2D
+@export var maxAmountPrStack: int
---
+++ Scripts/inventorySlot.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name InventorySlot
+
+@export var item: InventoryItems
+@export var amount: int
