80f893c406faac9b8121370db5240f81c8138419
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-10-31 10:42:08 -0300

    💾Feat: add Shader Transition in change Scenes

--- Scripts/scene_manager.gd
+++ Scripts/scene_manager.gd
@@ -1,5 +1,6 @@
-class_name SceneManager extends Node
+class_name SceneManager extends CanvasLayer
 
+@onready var animation: AnimationPlayer = $TransitionAnimation
 var player: Player
 var last_scene_name: String
 
@@ -11,5 +12,10 @@ func change_scene(from, to_scene_name: String) -> void:
 	player = from.player
 	player.get_parent().remove_child(player)
 	
+	animation.play("transition_out")
+	await animation.animation_finished
+	
 	var full_path = scene_dir_path + to_scene_name + ".tscn"
 	from.get_tree().call_deferred("change_scene_to_file", full_path)
+	
+	animation.play("transition_in")
